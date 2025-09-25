23355e7289178945beaad51c082bcab4341bf5a1
B735244 <dig05252@gmail.com>
2025-09-25 09:23:45 +0900

    no message

--- 11. SPRING BOOT/12RESTFUL_API/src/main/java/com/example/demo/Restful/Kakao/KakaoLoginController.java
+++ 11. SPRING BOOT/12RESTFUL_API/src/main/java/com/example/demo/Restful/Kakao/KakaoLoginController.java
@@ -17,10 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
-import javax.lang.model.element.Element;
 
-
-import javax.lang.model.element.Element;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
