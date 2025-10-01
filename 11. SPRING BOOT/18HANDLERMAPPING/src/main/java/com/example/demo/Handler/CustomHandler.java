8f3c6a896a79923615c7674b2831aee505d5def6
B735244 <dig05252@gmail.com>
2025-10-01 17:16:09 +0900

    10월 1일 수요일 비교적 덜 중요함.

---
+++ 11. SPRING BOOT/18HANDLERMAPPING/src/main/java/com/example/demo/Handler/CustomHandler.java
@@ -0,0 +1,21 @@
+package com.example.demo.Handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class CustomHandler implements Controller{
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("CUSTOMHANDLER's handleRequest invoke!!");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("memo/add");
+        return modelAndView;
+    }
+
+    public void HelloWorld(){
+        System.out.println("CUSTOMHANDLER's HelloWorld Method Invoke...!");
+    }
+}
