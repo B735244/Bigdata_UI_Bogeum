856483eb49f140ce77499f0ef2383b1b267ba8bb
B735244 <dig05252@gmail.com>
2025-10-16 17:00:02 +0900

    no message

--- 11. SPRING BOOT/19SECURITY/03_JWT_TOKEN/src/main/java/com/example/demo/config/auth/jwt/JWTAuthorizationFilter.java
+++ 11. SPRING BOOT/19SECURITY/03_JWT_TOKEN/src/main/java/com/example/demo/config/auth/jwt/JWTAuthorizationFilter.java
@@ -1,11 +1,15 @@
 package com.example.demo.config.auth.jwt;
 
+import com.example.demo.domain.entity.JwtToken;
+import com.example.demo.domain.repository.JwtTokenRepository;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,56 +18,102 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 
-@Slf4j
 @Component
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     @Autowired
     JWTTokenProvider jwtTokenProvider;
+    @Autowired
+    JwtTokenRepository jwtTokenRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //access-token 쿠키 받기
+        //전
+
         System.out.println("[JWTAuthorizationFilter] doFilterInternal ...");
-        String token = null;
-        token = Arrays.stream(request.getCookies())
-                        .filter((cookie)->{return cookie.getName().equals(JWTProperties.ACCESS_TOKEN_COOKIE_NAME);})
-                        .findFirst()
-                        .map((cookie)->{return cookie.getValue();})
-                        .orElse(null);
+        //access-token 쿠키 받기
+        String token = null;      //access-token 쿠키 받아 token=null;
 
-        System.out.println("Token: "+token);
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null)
+        {
+            token = Arrays.stream(cookies)
+                    .filter((cookie)->{return cookie.getName().equals(JWTProperties.ACCESS_TOKEN_COOKIE_NAME);})
+                    .findFirst()
+                    .map((cookie)->{return cookie.getValue();})
+                    .orElse(null);
+        }
+
+
+        System.out.println("TOKEN : " + token );
         if(token!=null){
-            //access-token 받기 후 -> Authentication 생성이후 SecurityContextHolder에 저장
-
+            //access-token -> Authentication 생성이후 SecurityContextHolder에 저장
             //1)access-token 만료되었는지 확인
-            //1-1)access-token 만료시? refresh-token 만료 여부 확인
             try {
                 if (jwtTokenProvider.validateToken(token)) {
-                    //1-1)access-token 만료 안되었을 시? Authentication 생성
+                    //1-1) access-token==만료 x ? Authentication 생성
                     Authentication authentication = jwtTokenProvider.getAuthentication(token);
-                    if(authentication!=null){
+                    if(authentication!=null)
                         SecurityContextHolder.getContext().setAuthentication(authentication);
-                    }
-
                 }
             }catch(ExpiredJwtException e){
-                //1-1)access-token 만료시? refresh-token 만료 여부 확인
+                //1-2) access-token==만료 o ? refresh-token 만료 여부확인
+                System.out.println("ExpiredJwtException ...AccessToken Expired.."  + e.getMessage());
+                //2) RefreshToken의 만료유무
+                JwtToken entity = jwtTokenRepository.findByAccessToken(token);
+                if(entity!=null){
 
-                log.info("ExpiredJwtException...!!!"+e.getMessage());
-            }catch(Exception e){
-                //1-1)access-token 만료시? refresh-token 만료 여부 확인
+                    try {
+                        if (jwtTokenProvider.validateToken(entity.getRefreshToken())) {
+                            //2-1) RefreshToken!=만료 ? -> AccessToken 재발급 -> 쿠키전달 + DB Token Info 갱신
+                            //AccessToken 재발급
+                            long now = (new Date()).getTime();  //현재시간
+                            String accessToken = Jwts.builder()
+                                    .setSubject(entity.getUsername()) //본문 TITLE
+                                    .setExpiration(new Date(now + JWTProperties.ACCESS_TOKEN_EXPIRATION_TIME )) //만료날짜(밀리초단위)
+                                    .signWith(jwtTokenProvider.getKey(), SignatureAlgorithm.HS256) // 서명값
+                                    .claim("username",entity.getUsername()) // 본문 내용
+                                    .claim("auth",entity.getAuth()) // 본문 내용 "ROLE_USER,USER_ADMIN"
+                                    .compact();
+                            //쿠키로 전달
+                            Cookie cookie = new Cookie(JWTProperties.ACCESS_TOKEN_COOKIE_NAME,accessToken);
+                            cookie.setMaxAge(JWTProperties.ACCESS_TOKEN_EXPIRATION_TIME);    //accesstoken 유지시간
+                            cookie.setPath("/");    //쿠키 적용경로(/ : 모든경로)
+                            response.addCookie(cookie); //응답정보에 쿠키 포함
+                            //DB's AccessToken 값 갱신
+                            entity.setAccessToken(accessToken);
+                            jwtTokenRepository.save(entity);
+
+
+                        }
+                    }catch(ExpiredJwtException e2){
+                        //2-2) RefreshToken==만료 ? -> DB's Token Info 삭제
+                        System.out.println("ExpiredJwtException ...RefreshToken Expired.."  + e2.getMessage());
+                    }catch (Exception e2){
+
+                    }
+
+
+
+
+                }
+
+            }catch (Exception e){
 
             }
-            }
 
 
-        filterChain.doFilter(request,response);
+
+            //2)
+        }else {
 
         }
 
+        filterChain.doFilter(request,response);
         
         //후
         
     }
-
+}
